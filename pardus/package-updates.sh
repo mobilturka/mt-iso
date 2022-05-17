6b36637df0a81a88638a8eee08a0a24aa1b86f63
mobilturka <93243536+mobilturka@users.noreply.github.com>
2022-05-17 11:31:15 +0300

    Update package-updates.sh

--- pardus/package-updates.sh
+++ pardus/package-updates.sh
@@ -1,9 +1,5 @@
 apt update
 apt install wget bzip2 zenity -y
 apt full-upgrade -y
-apt purge libreoffice-common -y
-apt install libreoffice-latest -y
-apt purge firefox-esr -y
 apt install librewolf -y
-apt purge gdebi packagekit xterm xfce4-sensors-plugin vlc -y
 apt autoremove -y
