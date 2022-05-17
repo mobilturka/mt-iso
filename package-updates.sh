a5b1bf88231f96f24966f2e38343d25b0cb3955b
mobilturka <93243536+mobilturka@users.noreply.github.com>
2022-05-17 10:13:44 +0300

    Create package-updates.sh

---
+++ package-updates.sh
@@ -0,0 +1,10 @@
+apt update
+apt install wget bzip2 zenity -y
+apt full-upgrade -y
+apt purge libreoffice-common -y
+apt install libreoffice-latest -y
+apt purge firefox-esr -y
+apt install librewolf -y
+apt install mpv -y
+apt purge gdebi packagekit xterm xfce4-sensors-plugin vlc xarchiver -y
+apt autoremove -y
