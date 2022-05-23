90f7fe7741d785744b380b3a071a73a814ce9880
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-23 18:30:15 +0300

    Update replace-kernel.sh

--- pardus/replace-kernel.sh
+++ pardus/replace-kernel.sh
@@ -1,6 +1,6 @@
-apt-get install -t yirmibir-backports linux-image-amd64
 echo "deb https://deb.debian.org/debian sid main contrib non-free" > /etc/apt/sources.list.d/sid.list
 apt-get update -y
 apt-get install gnome-session gnome-shell gnome-applets gnome-control-center mutter gjs adwaita-icon-theme -y
+apt-get install linux-image-amd64 -y
 echo "#deb https://deb.debian.org/debian sid main contrib non-free" > /etc/apt/sources.list.d/sid.list
 apt-get update -y
