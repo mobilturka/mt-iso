41a26501c8f56b2917ed8f93cfb5573401613376
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-23 18:22:35 +0300

    Update replace-kernel.sh

--- pardus/replace-kernel.sh
+++ pardus/replace-kernel.sh
@@ -3,3 +3,9 @@ echo "deb http://liquorix.net/debian stable main" > /etc/apt/sources.list.d/liqu
 apt-get update -y
 apt-get install linux-image-liquorix-amd64 -y
 apt-get install linux-headers-liquorix-amd64 -y
+
+echo "deb https://deb.debian.org/debian sid main contrib non-free" > /etc/apt/sources.list.d/sid.list
+apt-get update -y
+apt-get install gnome-session gnome-shell gnome-applets gnome-control-center mutter gjs adwaita-icon-theme -y
+echo "#deb https://deb.debian.org/debian sid main contrib non-free" > /etc/apt/sources.list.d/sid.list
+apt-get update -y
