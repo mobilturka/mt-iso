699f28863afa18236b36e942bff8a6956a83e080
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-23 13:35:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ sudo apt install git xorriso grub-pc-bin grub-efi mtools make python3 \
 make && make install
 ln -s sid /usr/share/debootstrap/scripts/unstable || true
 cd ../
-mkteaiso --profile=./pardus --output=/output/ --debug 2>&1
+mkteaiso --profile=./debian --output=/output/ --debug 2>&1
