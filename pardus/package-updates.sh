f85277aa345e183e153498dd5a8ea655026266bc
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-23 18:03:49 +0300

    Update package-updates.sh

--- pardus/package-updates.sh
+++ pardus/package-updates.sh
@@ -5,7 +5,7 @@ apt remove termit xterm icedtea-netx -y
 apt autoremove -y
 echo 'deb https://deb.debian.org/debian sid main contrib non-free' > /etc/apt/sources.list.d/sid.list
 apt update
-apt -t sid install gnome-session gnome-shell gnome-applets gnome-control-center mutter gjs adwaita-icon-theme
+apt -t sid install gnome-session gnome-shell gnome-applets gnome-control-center mutter gjs adwaita-icon-theme -y
 echo '#deb https://deb.debian.org/debian sid main contrib non-free' > /etc/apt/sources.list.d/sid.list
 apt update
 apt autoremove -y
