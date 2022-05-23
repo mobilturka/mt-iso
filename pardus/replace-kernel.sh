e449f49e82a72e5b4b5d6553dae203a2f80e92b9
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-23 18:26:50 +0300

    Update replace-kernel.sh

--- pardus/replace-kernel.sh
+++ pardus/replace-kernel.sh
@@ -1,9 +1,5 @@
-wget -O - https://liquorix.net/liquorix-keyring.gpg | apt-key add -
-echo "deb http://liquorix.net/debian stable main" > /etc/apt/sources.list.d/liquorix.list
-apt-get update -y
-apt-get install linux-image-liquorix-amd64 -y
-apt-get install linux-headers-liquorix-amd64 -y
 
+apt-get install -t yirmibir-backports linux-image-amd64
 echo "deb https://deb.debian.org/debian sid main contrib non-free" > /etc/apt/sources.list.d/sid.list
 apt-get update -y
 apt-get install gnome-session gnome-shell gnome-applets gnome-control-center mutter gjs adwaita-icon-theme -y
