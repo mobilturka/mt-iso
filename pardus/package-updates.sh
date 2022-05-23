63c80ec3d6f3199c01e3d48f9ae18789db2d0195
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-23 18:19:16 +0300

    Update package-updates.sh

--- pardus/package-updates.sh
+++ pardus/package-updates.sh
@@ -3,11 +3,6 @@ apt install wget bzip2 zenity -y
 apt full-upgrade -y
 apt remove termit xterm icedtea-netx -y
 apt autoremove -y
-echo 'deb https://deb.debian.org/debian sid main contrib non-free' > /etc/apt/sources.list.d/sid.list
-apt update
-apt -t sid install gnome-session gnome-shell gnome-applets gnome-control-center mutter gjs adwaita-icon-theme -y
-echo '#deb https://deb.debian.org/debian sid main contrib non-free' > /etc/apt/sources.list.d/sid.list
-apt update
-apt autoremove -y
+
 
 
