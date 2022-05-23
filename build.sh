87e3c1aa78641e98e336416f82cfa6d3e96a6c60
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-23 20:58:06 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,31 @@
 #!/bin/bash
 set -e
-#Gerekli paketlerin kurulması
+#### Check root
+if [[ ! $UID -eq 0 ]] ; then
+    echo -e "\033[31;1mYou must be root!\033[:0m"
+    exit 1
+fi
+#### Remove all environmental variable
+for e in $(env | sed "s/=.*//g") ; do
+    unset "$e" &>/dev/null
+done
+
+#### Set environmental variables
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin
+export LANG=C
+export SHELL=/bin/bash
+export TERM=linux
+export DEBIAN_FRONTEND=noninteractive
+
+#### Install dependencies
+if which apt &>/dev/null && [[ -d /var/lib/dpkg && -d /etc/apt ]] ; then
+    apt-get update
+    apt-get install curl mtools squashfs-tools grub-pc-bin grub-efi xorriso debootstrap -y
+fi
+
+set -ex
+#### Chroot create
+mkdir debian-chroot || true
 apt-get install debootstrap xorriso squashfs-tools mtools grub-pc-bin grub-efi devscripts -y
 
 #debian-chroot dosyası oluşturalım
