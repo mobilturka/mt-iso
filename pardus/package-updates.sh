5febdf924db2cfac8fa2b5efd4f4f63a2b56f115
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-24 08:45:00 +0300

    Update package-updates.sh

--- pardus/package-updates.sh
+++ pardus/package-updates.sh
@@ -1,6 +1,6 @@
 apt update
 apt install wget bzip2 zenity -y
 apt full-upgrade -y
-apt remove firefox-esr termit xterm icedtea-netx -y
-apt install librewolf
+apt remove firefox-esr termit xterm icedtea-netx xarchiver -y
+apt install librewolf file-roller
 apt autoremove -y
