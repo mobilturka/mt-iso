2138e86bb3eea7fccedddea97c0e6b6b090a71fd
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-27 19:07:30 +0300

    Update package-updates.sh

--- pardus/package-updates.sh
+++ pardus/package-updates.sh
@@ -1,8 +1,8 @@
 apt update
 apt install wget bzip2 zenity -y
 apt full-upgrade -y
-apt purge libreoffice-common firefox-esr -y
-apt purge gdebi packagekit pardus-java-installer evolution-common evolution-data-server-common xterm xfce4-sensors-plugin vlc xarchiver -y
-apt purge audacious audacious-plugins-data drawing firefox-esr gimp gimp-data -y
+apt purge libreoffice-common -y
+apt remove gdebi packagekit pardus-java-installer evolution-common evolution-data-server-common xterm xfce4-sensors-plugin vlc xarchiver -y
+apt remove firefox-esr audacious audacious-plugins-data drawing firefox-esr gimp gimp-data -y
 apt install librewolf -y
 apt autoremove -y
