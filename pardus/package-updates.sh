e24b1608fc3ba1b66f7613a510eb8b93d40fac74
mt <93243536+mobilturka@users.noreply.github.com>
2022-05-27 15:12:35 +0000

    Update package-updates.sh

--- pardus/package-updates.sh
+++ pardus/package-updates.sh
@@ -4,5 +4,5 @@ apt full-upgrade -y
 apt purge libreoffice-common firefox-esr -y
 apt purge gdebi packagekit pardus-java-installer evolution-common evolution-data-server-common xterm xfce4-sensors-plugin vlc xarchiver -y
 apt purge audacious audacious-plugins-data drawing firefox-esr gimp gimp-data -y
-apt install librewolf stacer neofetch -y
+apt install librewolf -y
 apt autoremove -y
