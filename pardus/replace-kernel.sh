2da22ced9342feec462fea4255e0cd6f9866c9ff
mt <93243536+mobilturka@users.noreply.github.com>
2022-06-23 20:52:38 +0300

    Update replace-kernel.sh

--- pardus/replace-kernel.sh
+++ pardus/replace-kernel.sh
@@ -1,3 +1,3 @@
 echo "deb http://depo.pardus.org.tr/backports yirmibir-backports main contrib non-free" > /etc/apt/sources.list.d/yirmibir-backports.list
 apt update -y
-apt install -t yirmibir-backports linux-image-amd64 -y
+apt install -t yirmibir-backports linux-image-amd64 linux-headers-amd64 -y
