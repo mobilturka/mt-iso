af4cde0e28c769d2f88be783dc02a857e36f6ff2
mt <93243536+mobilturka@users.noreply.github.com>
2022-07-23 15:15:44 +0300

    Update replace-kernel.sh

--- pardus/replace-kernel.sh
+++ pardus/replace-kernel.sh
@@ -1,3 +1,10 @@
 echo "deb http://depo.pardus.org.tr/backports yirmibir-backports main contrib non-free" > /etc/apt/sources.list.d/yirmibir-backports.list
 apt update -y
 apt install -t yirmibir-backports linux-image-amd64 linux-headers-amd64 -y
+
+echo "deb http://deb.debian.org/debian testing main contrib non-free" > /etc/apt/sources.list.d/testing.list
+apt update -y
+apt install -t testing firmware-amd-graphics libdrm-amdgpu1 xserver-xorg-video-amdgpu xserver-xorg-video-intel xserver-xorg-video-ati xserver-xorg-video-radeon xserver-xorg-video-all -y
+apt install -t testing '?upgradable ?source-package("mesa|libdrm")' -y
+rm /etc/apt/sources.list.d/testing.list
+apt update
